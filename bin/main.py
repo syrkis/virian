4062fb757851e221a77718e92fe0687878bb57c2
Noah Syrkis <noah@syrkis.com>
2021-09-07 19:06:43 +0200

    update main for singularity

--- bin/main.py
+++ bin/main.py
@@ -1,15 +1,2 @@
-from csv import writer
-from tqdm import tqdm
-from transformers import DistilBertTokenizerFast
-
-tokenizer = DistilBertTokenizerFast.from_pretrained('distilbert-base-uncased', model_max_len=512)
-data = open('data/raw.tar', 'r').readlines()
-batch_size = 2 ** 14
-
-with open('data/tok.csv', 'a+', newline="") as f:
-    writer = writer(f)
-    for i in tqdm(range(0, (len(data) // batch_size) * batch_size, batch_size)):
-        batch = tokenizer.batch_encode_plus(data[i: i + batch_size], truncation=True)['input_ids']
-        writer.writerows(batch)
-        
-
+import train
+train.main()
