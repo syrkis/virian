b60a125b662cad823005681bf7304dc622fcfab4
Noah Syrkis <noah@syrkis.com>
2021-09-07 19:56:06 +0200

    prep for article description

--- bin/main.py
+++ bin/main.py
@@ -1,2 +1,27 @@
-import train
-train.main()
+# main.py
+#   describes articles relative to haidt dimensions  
+# by: Noah Syrkis
+
+# imports
+import torch
+from dataset import Dataset
+from model import Model
+
+
+# describe articles on haidth dimensions
+def describe(model, articles):
+    for article in articles:
+        _, comp = model.foward(article)
+
+
+# call stack
+def main():
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    ds = Dataset(device, None, 2 ** 13)
+    model = Model(ds.model.distilbert.embeddings.word_embeddings.weight.shape[1]).to(device)
+    model.load_state_dict(torch.load('..ccodels/model.pt'))
+    model.eval()
+    describe(model, article) 
+
+if __name__ == '__main__':
+    main()
