86dcad9f42da2702d5efe1586c2e95654643ed90
Noah Syrkis <noah@syrkis.com>
2022-01-25 23:47:17 -0300

    undo ignore txt and rm R

--- docs/pew.R
+++
@@ -1,9 +0,0 @@
-# imports
-library("haven")
-library(plyr)
-
-# dataset files
-setwd("~/virian/data/pew")
-folders <- dir()
-file <- paste(folders[1], list.files(path=folders[1], pattern='*.sav'), sep='/')
-data <- read_sav(file)
