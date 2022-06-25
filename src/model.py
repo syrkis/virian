27b32498f74a561cf401dbdf21de4211fa3fcb8a
Noah Syrkis <noah@syrkis.com>
2022-06-25 14:43:59 +0200

    weigh weight

--- src/model.py
+++ src/model.py
@@ -9,32 +9,26 @@ from torch import nn
 # define the model
 class Model(nn.Module):
     def __init__(self, params):
-        self.params = params
         super(Model, self).__init__()
-        self.lstm = nn.queueSTM(
-            input_size,
-            hidden_size,
-            batch_first=True,
-            bidirectional=True
-        )
-        self.fc = nn.Linear(hidden_size * 2, output_size)
+        self.params = params
+        self.weight = nn.Parameter(torch.rand(1000))
 
     def forward(self, x, w):
+        print(x.shape, w.shape)
         z = self.encode(x)
         y = self.infer(z, w)
         x = self.decode(z)
         return x, y
 
     def infer(self, z, w): # make LSTM or attention
+        w = w * self.wigh
         return z
 
     def encode(self, x):
-        return queue
-
+        return x
 
     def decode(self, z):
         return z
-
 
 # call the model
 def main():
